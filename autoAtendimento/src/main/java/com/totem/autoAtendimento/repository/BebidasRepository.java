29c90157c6259a07c959505efabc413980f7cea1
mateus-damasceno-zup <mateus.damasceno@zup.com.br>
2023-08-02 20:35:24 -0300

    Revert "bebida ok service, controller, repo e model, todo view"
    This reverts commit 6f8500c0463de29c9ff97662303e2ad631b1901b.

--- autoAtendimento/src/main/java/com/totem/autoAtendimento/repository/BebidasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.totem.autoAtendimento.repository;
-
-import com.totem.autoAtendimento.model.Bebidas;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BebidasRepository extends CrudRepository<Bebidas,Long> {
-}
--- autoAtendimento/src/main/java/com/totem/autoAtendimento/repository/CarrinhoLanchesBebidas.java
+++
@@ -1,9 +0,0 @@
-/*
-package com.totem.autoAtendimento.repository;
-
-import com.totem.autoAtendimento.model.CarrinhoLancheBebidaPk;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarrinhoLanchesBebidas extends CrudRepository<CarrinhoLanchesBebidas, CarrinhoLancheBebidaPk> {
-}
-/*/
